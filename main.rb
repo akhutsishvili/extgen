a505ad5df0a36b67b123f8bc93d8a20276f16aef
Aleksandre Khutsishvili <a.khutsishvili@emis.ge>
2016-04-27 16:18:43 +0400

    little bit of refactoring and introduce chaining

--- main.rb
+++ main.rb
@@ -89,45 +89,46 @@ class Store
   end
 end
 
-class Element
+class ComboBox
+  @@path = ""
+  @@params = ""
   @@constructor_code = [
     "    constructor: function (cfg) {",
     "       cfg = cfg || {}",
     "       var me = this",
     "    }"
   ]
-  def combo(path, name, params)
+  def initialize(path, params)
+    @@path = path
+    @@params = params
     utils = Utils.new
     element_alias = utils.path_to_alias path
-    file_name = utils.path_to_file(path, "view")
-    code = [
+    @code = [
       "Ext.define('#{$config["project_name"]}.view.#{path}'') {",
       "    fieldLabel: '',",
       "    extend: 'Ext.form.ComboBox',",
       "    alias: 'widget.#{element_alias}'",
       "    name: ''"
     ]
-
-
-    if params.include? "-c"
-      code.push @@constructor_code
+    if @@params.include? "-c"
+      @code.push @@constructor_code
     end
-    
-    if params.include? "-s"
-      Store.new().create(path, params)
-    end
-
-    code.push "})"
-
-    # generate
-    utils.generate(path, "view", code)
+    @code.push "})"
+    self
   end
 
+  def create()
+    if @@params.include? "-s"
+      Store.new().create(@@path, @@params)
+    end
+    
+    Utils.new().generate(@@path, "view", @code)
+    self
+  end
 end
-
 
 utils = Utils.new()
 params = utils.argv_parser
 if params[:type] == "combo"
-  Element.new().combo(params[:path], params[:name], params[:options])
+  ComboBox.new(params[:path], params[:options]).create()
 end
