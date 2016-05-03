45ec0e8cf28f1594371b4aabf21535b509a2eb2f
Aleksandre Khutsishvili <a.khutsishvili@emis.ge>
2016-05-03 15:11:03 +0400

    add grid and grid fields generator

--- main.rb
+++ main.rb
@@ -47,6 +47,15 @@ class Utils
     }
   end
 
+  def options_equality_parser(options)
+    equalited_map = {}
+    contains_equality = options.select do |o| o.include? "=" end
+    contains_equality.each do |element|
+      parsed = element.split("=")
+      equalited_map[parsed[0]] = parsed[1]
+    end
+    equalited_map
+  end
 end
 
 class Model
@@ -106,7 +115,7 @@ class ComboBox
     utils = Utils.new
     element_alias = utils.path_to_alias path
     @code = [
-      "Ext.define('#{$config["project_name"]}.view.#{path}'') {",
+      "Ext.define('#{$config["project_name"]}.view.#{path}') {",
       "    fieldLabel: '',",
       "    extend: 'Ext.form.ComboBox',",
       "    alias: 'widget.#{element_alias}'",
@@ -131,8 +140,70 @@ class ComboBox
   end
 end
 
+class Grid
+  @@path = ""
+  @@params = ""
+  @@constructor_code = [
+    "    constructor: function (cfg) {",
+    "       cfg = cfg || {}",
+    "       var me = this",
+    "    }"
+  ]
+  
+  def initialize(path, options)
+    @@path = path
+    @@options = options
+    utils = Utils.new
+    element_alias = utils.path_to_alias path
+    @code = [
+      "Ext.define('#{$config["project_name"]}.view.#{path}') {",
+      "    title: '',",
+      "    extend: 'Ext.grid.Panel',",
+      "    alias: 'widget.#{element_alias}'",
+    ]
+
+    if @@options.include? "-s"
+      store_path = Store.new().create(@@path, @@options)
+      @code.push("    store: {xclass: '#{store_path}'},")
+    end
+    
+    @code.push(create_fields options)
+    if @@options.include? "-c"
+      @code.push @@constructor_code
+    end
+    @code.push "})"
+    self
+  end
+
+  def create_fields(options)
+    fn_param = Utils.new().options_equality_parser(options)["-fn"]
+    field_number = fn_param.to_i
+    fields_code = []
+    if fn_param and field_number > 0
+      template = [
+        "{",
+        "    text: '',",
+        "    dataIndex: ''",
+        "},"
+      ]
+      field_number.times do fields_code.push template end
+      "    fields: [" + fields_code.join("\n").chomp(",") + "],"
+    else
+      "    fields: [],"
+    end
+  end
+
+  def create()
+    Utils.new().generate(@@path, "view", @code)
+    self
+  end
+end
+
 utils = Utils.new()
-params = utils.argv_parser
-if params[:type] == "combo"
-  ComboBox.new(params[:path], params[:options]).create()
+options = utils.argv_parser
+if options[:type] == "combo"
+  ComboBox.new(options[:path], options[:options]).create()
+end
+if options[:type] == "grid"
+  Grid.new(options[:path], options[:options]).create()
 end
