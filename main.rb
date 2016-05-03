7a74237cfc6072c96eb864f8b486f412d07a3a65
Aleksandre Khutsishvili <a.khutsishvili@emis.ge>
2016-05-03 15:31:33 +0400

    move Store to separate file

--- main.rb
+++ main.rb
@@ -2,37 +2,10 @@ require 'yaml'
 $config = YAML.load_file('extgen_config.yml')
 
 require_relative "Utils"
-require_relative "Model"
+require_relative "Utils"
+require_relative "Store"
 
-class Store
 
-  def create(path, params)
-    utils = Utils.new
-    store_path = "#{$config["project_name"]}.store.#{path}"
-    model_path = "'Ext.data.Model'"
-    
-    if params.include? "-m"
-      model_path = Model.new().create(path, params)
-    end
-
-    code = [
-      "Ext.define('#{store_path}') {",
-      "    extend: 'Ext.data.Store',",
-      "    model:  Ext.create(#{model_path}),",
-      "    proxy: {",
-      "        type: 'ajax',",
-      "        url: 'rest/',",
-      "        reader: {",
-      "            type: 'json'",
-      "        }",
-      "    }",
-      "})"
-    ]
-    utils.generate(path, "store", code)
-
-    store_path
-  end
-end
 
 class ComboBox
   @@path = ""
