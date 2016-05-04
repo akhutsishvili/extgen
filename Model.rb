11667719a7c848c76829d30b654188d080864342
Aleksandre Khutsishvili <a.khutsishvili@emis.ge>
2016-05-04 15:19:54 +0400

    refactor model class

--- Model.rb
+++ Model.rb
@@ -1,19 +1,29 @@
 require_relative "Utils"
 
 class Model
-  def create(path, params)
-    utils = Utils.new
-    model_path = "'#{$config["project_name"]}.model.#{path}'"
-    code = [
-      "Ext.define(#{model_path}, {",
+  @model_definition = nil
+  @code = nil
+
+  def initialize(path, params)
+    @model_definition = "'#{$config["project_name"]}.model.#{path}'"
+    @code = [
+      "Ext.define(#{@model_definition}, {",
       "    extend: 'Ext.data.Model',",
       "    fields: [{",
       "        name: 'id',",
       "        type: 'int'",
       "    }]",
       "})"
-    ]
+    ] 
+    self
+  end
+
+  def get_model_definition
+    @model_definition
+  end
+  
+  def create
+    utils = Utils.new
     utils.generate(path, "model", code)
-    model_path
   end
 end
