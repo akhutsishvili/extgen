0f7d52ad858724a3594823b051ceddef7e849c5f
Khutishsvili Alex <khutsishvili.alex@gmail.com>
2016-05-18 22:46:01 +0400

    now return full path of store

--- Script.rb
+++ Script.rb
@@ -8,7 +8,7 @@ class Script
   def create_store
     path =  @generator.instance_variable_get(:@path)
     o = @generator.instance_variable_get(:@options)
-    Generator.new('store', path, o).generate()
+    Generator.new('store', path, o).generate().create().get_full_path
     # create model
     # if @generator.option? "-m"
     #   self.create_model
