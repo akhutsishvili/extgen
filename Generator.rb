93cc42377c3e7242568964de0bcbbc647bf6144f
Khutishsvili Alex <khutsishvili.alex@gmail.com>
2016-05-18 22:23:32 +0400

    add generator class, strat introducing templating system

---
+++ Generator.rb
@@ -0,0 +1,53 @@
+require "erb"
+require_relative "#{$script_location}/Utils"
+
+class Generator
+  @tpl = nil
+  @tpl_type = nil
+  @path = nil
+  @options = nil
+  @code = nil
+  @tpl_file = nil
+  @params = nil
+  @element_alias = nil
+  @store = nil
+  @model = nil
+  @eq_options = nil
+
+  def initialize(tpl, path, options)
+    u = Utils.new
+    @params = u.parse_argv
+    @tpl = tpl.capitalize
+    @path = path
+    @options = options
+    @element_alias = u.path_to_alias @path
+    @eq_options = u.options_equality_parser options
+    @tpl_type = u.get_tpl_type @tpl
+  end
+
+  def set_tpl_file
+    @tpl_file = "#{$script_location}/templates/#{@tpl}.erb"
+  end
+  
+  def generate_code_from_template
+    template = File.read @tpl_file
+    renderer = ERB.new(template)
+
+    @code = renderer.result(binding)
+  end
+
+  def option? option
+    @options.include? option
+  end
+  
+  def generate
+    self.set_tpl_file
+    self.generate_code_from_template
+    self
+  end
+
+  def create
+    Utils.new().create_file(@path, @tpl_type, @code)
+  end
+  
+end
