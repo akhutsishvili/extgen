2358b230a70664e687c45f5f468ad829ae7171c0
Khutishsvili Alex <khutsishvili.alex@gmail.com>
2016-05-18 22:23:49 +0400

    add scripts class

---
+++ Script.rb
@@ -0,0 +1,21 @@
+class Script
+  @generator = nil
+
+  def initialize g
+    @generator = g
+  end
+  
+  def create_store
+    path =  @generator.instance_variable_get(:@path)
+    o = @generator.instance_variable_get(:@options)
+    Generator.new('store', path, o).generate()
+    # create model
+    # if @generator.option? "-m"
+    #   self.create_model
+    # end
+  end
+
+  def create_model
+
+  end
+end
